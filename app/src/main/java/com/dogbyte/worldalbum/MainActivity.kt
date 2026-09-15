ea39e370c2cb70a74f2aae47cde657198ddaaa0e
Taylor Christian Mattheisen <joshoshfield@gmail.com>
2026-09-14 20:09:38 -0700

    Drop appcompat: framework Activity + Theme.NoTitleBar.Fullscreen, zero deps (kotlin-stdlib duplicate-class gate)

--- app/src/main/java/com/dogbyte/worldalbum/MainActivity.kt
+++ app/src/main/java/com/dogbyte/worldalbum/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.dogbyte.worldalbum
 
+import android.app.Activity
 import android.os.Bundle
-import android.webkit.WebView
 import android.webkit.WebSettings
-import androidx.appcompat.app.AppCompatActivity
+import android.webkit.WebView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     private lateinit var web: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
